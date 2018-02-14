67c940cc0277c85648e9377f6c0cdf968d761e75
Yuragranovsky <yuragranovsky@gmail.com>
2018-02-14 09:21:45 +0200

    classwork #35

---
+++ 18_02_06_CarsEmbedded/src/telran/cars/controller/CarsRandomAppl.java
@@ -0,0 +1,17 @@
+package telran.cars.controller;
+
+import telran.cars.model.IRentCompany;
+import telran.cars.model.RentCompanyEmbedded;
+
+public class CarsRandomAppl {
+
+	public static void main(String[] args) {
+		IRentCompany rentCompany=new RentCompanyEmbedded();
+		RandomRentCompanyController controller=
+		new RandomRentCompanyController(rentCompany);
+		controller.createRentCompany(3);
+		rentCompany.getAllRecords().limit(200).forEach(System.out::println);
+
+	}
+
+}
