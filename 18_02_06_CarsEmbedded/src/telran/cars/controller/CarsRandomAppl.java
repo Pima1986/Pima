2ced7498303b860216f0aa4c0814e1357ae06419
Yuragranovsky <yuragranovsky@gmail.com>
2018-02-14 11:49:24 +0200

    years from 4 to 2

--- 18_02_06_CarsEmbedded/src/telran/cars/controller/CarsRandomAppl.java
+++ 18_02_06_CarsEmbedded/src/telran/cars/controller/CarsRandomAppl.java
@@ -9,7 +9,7 @@ public class CarsRandomAppl {
 		IRentCompany rentCompany=new RentCompanyEmbedded();
 		RandomRentCompanyController controller=
 		new RandomRentCompanyController(rentCompany);
-		controller.createRentCompany(4);
+		controller.createRentCompany(2);
 		rentCompany.getAllRecords().limit(200).forEach(System.out::println);
 
 	}
